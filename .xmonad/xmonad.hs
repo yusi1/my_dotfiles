1a5d864f65b953cd1d9581997394c539438a6378
Yusef Aslam <newdotfiles@protonmail.com>
2021-02-23 09:50:29 +0000

    Updated

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -86,6 +86,7 @@ myManageHook = composeAll         -- Add Custom Hook to make certain windows ope
       , className =? "Alacritty" --> doShift ( myWorkspaces !! 0 ) <+> viewShift ( myWorkspaces !! 0 )
       , (className =? "Steam" <&&> resource =? "Dialog") --> doFloat
       , className =? "mpv"     --> doShift ( myWorkspaces !! 5 ) <+> viewShift ( myWorkspaces !! 5 )
+      , className =? "vlc"     --> doShift ( myWorkspaces !! 5 ) <+> viewShift ( myWorkspaces !! 5 )
       , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat  -- Float Firefox Dialog
       , (className =? "IceCat" <&&> resource =? "Dialog") --> doFloat  -- Float IceCat Dialog
       , className =? "IceCat"       --> doShift ( myWorkspaces !! 1 ) <+> viewShift ( myWorkspaces !! 1 )
