fb032da9f3c0d8710ff9b85a6cc21aa0bc06dd65
Yusef Aslam <newdotfiles@protonmail.com>
2021-02-25 11:30:43 +0000

    Added LibreOffice Dialog Float

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -91,6 +91,7 @@ myManageHook = composeAll         -- Add Custom Hook to make certain windows ope
       className =? "Steam"    --> ( doShiftWS 6 )
       , className =? "Alacritty" --> ( doShiftWS 0 )
       , (className =? "Steam" <&&> resource =? "Dialog") --> doFloat
+      , className =? "Soffice"  --> doFloat
       , className =? "mpv" --> ( doShiftWS 5 )
       , className =? "vlc" --> ( doShiftWS 5 )
       , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat  -- Float Firefox Dialog
