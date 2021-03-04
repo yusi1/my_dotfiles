40b7f7414caee4c65f4dcee93d1ce87dc0218b59
Yusef Aslam <newdotfiles@protonmail.com>
2021-03-04 09:09:14 +0000

    Updated a few things

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -268,7 +268,7 @@ main = do
 
             , ((mod1Mask .|. controlMask, xK_k), spawn "mousepad ~/Documents/keybinds.txt")  -- Show keybinds
             
-            , ((mod1Mask, xK_F11), spawn "killall stalonetray; stalonetray & disown") -- Restart Stalonetray
+            , ((mod1Mask, xK_F11), spawn "killall trayer; trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --transparent true --alpha 0 --tint 0x000000 --widthtype request --monitor 1 --height 24 & disown") -- Restart Trayer
             
             , ((mod1Mask .|. controlMask, xK_m), spawn "mailspring") -- spawn mail client
 
