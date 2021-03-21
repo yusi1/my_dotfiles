f0ed7d35f7186ba9a50e0fb224e1d22d917474ba
Yusef Aslam <newdotfiles@protonmail.com>
2021-03-21 20:34:06 +0000

    Tweaks

--- .p10k.zsh
+++ .p10k.zsh
@@ -87,15 +87,15 @@
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
-    # load                  # CPU load
+    #load                  # CPU load
     # disk_usage            # disk usage
-    # ram                   # free RAM
+    #ram                   # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     time                    # current time
-    # ip                    # ip address and bandwidth usage for a specified network interface
+    #ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
     # battery               # internal battery
